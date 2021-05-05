d2f2c05252c228fadf342e31c60a5aa2a6d24bd1
Neha <neha.oscprofessionals@gmail.com>
2021-05-06 01:36:47 +0530

    Add Location Logic

--- WheatherApp/Main/ViewController/MainViewController.swift
+++ WheatherApp/Main/ViewController/MainViewController.swift
@@ -6,21 +6,27 @@
 //
 
 import UIKit
+import CoreLocation
 
 class MainViewController: UIViewController {
 
     @IBOutlet weak var table:UITableView!
     
     var modals = [Wheather]()
+    let locationManager = CLLocationManager()
+    var currentLoaction:CLLocation?
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         table.register(HourlyCollectionViewCell.nib(), forCellReuseIdentifier: HourlyCollectionViewCell.identifier)
         table.register(WeatherTableViewCell.nib(), forCellReuseIdentifier: WeatherTableViewCell.identifier)
+        
     }
     
-
+    override func viewDidAppear(_ animated: Bool) {
+        setUpLocation()
+    }
 }
 
 extension MainViewController: UITableViewDelegate, UITableViewDataSource {
@@ -30,5 +36,31 @@ extension MainViewController: UITableViewDelegate, UITableViewDataSource {
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         return UITableViewCell()
+    }
+}
+//MARK: - Location
+extension MainViewController: CLLocationManagerDelegate {
+    
+    func setUpLocation() {
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if !locations.isEmpty , currentLoaction == nil {
+            currentLoaction = locations.first
+            locationManager.stopUpdatingLocation()
+            requestWeatherForLoaction()
+        }
+    }
+    
+    func requestWeatherForLoaction() {
+        guard let currentLoaction = currentLoaction else {
+            return
+        }
+        let long = currentLoaction.coordinate.longitude
+        let lat = currentLoaction.coordinate.latitude
+        print("long: \(long) lat:  \(lat)")
     }
 }
