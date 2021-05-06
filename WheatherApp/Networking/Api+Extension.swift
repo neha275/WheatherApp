5615d9bbb425cb97da9ec9b922fc80d41b1f0581
Neha <neha.oscprofessionals@gmail.com>
2021-05-07 00:36:55 +0530

    Add base url

--- WheatherApp/Networking/Api+Extension.swift
+++ WheatherApp/Networking/Api+Extension.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+
+extension API {
+    static let baseURLString = "api.openweathermap.org/data/2.5/"
+    
+    static func getURLFor(lat: Double, lon: Double) -> String {
+        return "\(baseURLString)onecall?lat=\(lat)&lon=\(lon)&exclude=minutely&appid=\(key)&units=imperial"
+    }
+    
+}
