fd5cd7513c7ce3377b58338672978e9053f9c73f
Neha <neha.oscprofessionals@gmail.com>
2021-05-07 01:05:47 +0530

    Api fetch request logic

--- WheatherApp/Networking/NetworkManager.swift
+++ WheatherApp/Networking/NetworkManager.swift
@@ -6,3 +6,43 @@
 //
 
 import Foundation
+
+final class NetworkManager<T: Codable>{
+    
+    static func fetch(for url: URL, completion: @escaping (Result<T, NetworkError>) -> Void) {
+        URLSession.shared.dataTask(with: url) { (data,response, error) in
+            guard error == nil else {
+                print(String(describing: error))
+                completion(.failure(.error(err: error!.localizedDescription)))
+                return
+            }
+            
+            guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+                completion(.failure(.invalidResponse))
+                return
+            }
+            guard let data = data else {
+                completion(.failure(.invalidData))
+                return
+            }
+            
+            do {
+                let json = try JSONDecoder().decode(T.self, from: data)
+                completion(.success(json))
+            }catch let err {
+                print((String.init(describing: err.localizedDescription)))
+                completion(.failure(.DecoadingError(err: err.localizedDescription)))
+            }
+        }.resume()
+    }
+    
+    
+}
+
+enum NetworkError: Error{
+ 
+    case invalidResponse
+    case invalidData
+    case error(err: String)
+    case DecoadingError(err: String)
+}
